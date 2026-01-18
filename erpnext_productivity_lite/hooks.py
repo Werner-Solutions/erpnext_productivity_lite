16899b7ad2b244fa8deeb0601ac0c5fe0d261593
Christian WERNER <cw@werner.solutions>
2026-01-18 18:04:23 +0100

    Add hooks.py, patches.txt, and package structure

---
+++ erpnext_productivity_lite/hooks.py
@@ -0,0 +1,82 @@
+app_name = "erpnext_productivity_lite"
+app_title = "Productivity Lite"
+app_publisher = "Werner.Solutions"
+app_description = "Todoist-style task management with gamification for ERPNext/Frappe"
+app_email = "office@werner.solutions"
+app_license = "MIT"
+required_apps = ["erpnext"]
+
+# App version
+app_version = "0.0.1"
+
+# Includes in <head>
+# ------------------
+
+# include js, css files in header of desk.html
+# app_include_css = "/assets/erpnext_productivity_lite/css/erpnext_productivity_lite.css"
+# app_include_js = "/assets/erpnext_productivity_lite/js/erpnext_productivity_lite.js"
+
+# include js, css files in header of web template
+# web_include_css = "/assets/erpnext_productivity_lite/css/erpnext_productivity_lite.css"
+# web_include_js = "/assets/erpnext_productivity_lite/js/erpnext_productivity_lite.js"
+
+# Home Pages
+# ----------
+
+# application home page (will override Website Settings)
+# home_page = "login"
+
+# website user home page (by Role)
+# role_home_page = {
+#	"Role": "home_page"
+# }
+
+# Generators
+# ----------
+
+# automatically create page for each record of this doctype
+# website_generators = ["Web Page"]
+
+# Jinja
+# ----------
+
+# add methods and filters to jinja environment
+# jinja = {
+#	"methods": "erpnext_productivity_lite.utils.jinja_methods",
+#	"filters": "erpnext_productivity_lite.utils.jinja_filters"
+# }
+
+# Installation
+# ------------
+
+# before_install = "erpnext_productivity_lite.install.before_install"
+# after_install = "erpnext_productivity_lite.install.after_install"
+
+# Uninstallation
+# ------------
+
+# before_uninstall = "erpnext_productivity_lite.uninstall.before_uninstall"
+# after_uninstall = "erpnext_productivity_lite.uninstall.after_uninstall"
+
+# Document Events
+# ---------------
+
+# doc_events = {
+#	"Task": {
+#		"on_update": "erpnext_productivity_lite.events.task_on_update",
+#	}
+# }
+
+# Scheduled Tasks
+# ---------------
+
+# scheduler_events = {
+#	"daily": [
+#		"erpnext_productivity_lite.tasks.daily"
+#	],
+# }
+
+# Testing
+# -------
+
+# before_tests = "erpnext_productivity_lite.install.before_tests"
